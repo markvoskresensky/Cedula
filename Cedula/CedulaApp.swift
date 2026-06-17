1760db71b7d30df1fd6f671fdbd41c6671657dd2
Marko <mark.voskresensky@gmail.com>
2026-06-17 13:40:57 +0200

    Initial commit: Xcode SwiftUI project skeleton

---
+++ Cedula/CedulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CedulaApp.swift
+//  Cedula
+//
+//  Created by Marko on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CedulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CedulaTests/CedulaTests.swift
@@ -0,0 +1,19 @@
+//
+//  CedulaTests.swift
+//  CedulaTests
+//
+//  Created by Marko on 16.06.2026.
+//
+
+import Testing
+@testable import Cedula
+
+struct CedulaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
